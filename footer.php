086fda5dabcaadd23af8f1251a5b64304da98d89
alex <alexandre.baudouin161@gmail.com>
2024-09-24 19:22:53 +0200

    MAJ mise en place footer + css

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer>
-    <a href=""><p>Conditions Générales d'Utilisation (CGU) du Site Web</p></a>
-    <a href=""><p>Me contacter</p></a>
-    <p>© 2024. Tous droits réservés.</p>   
+    <div class="footer">
+        <a href=""><p>Conditions Générales d'Utilisation (CGU) du Site Web</p></a>
+        <a href=""><p>Me contacter</p></a>
+        <p>© 2024. Tous droits réservés.</p> 
+    </div>  
 </footer>
