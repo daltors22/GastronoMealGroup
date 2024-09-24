f02e7173d77c209fe7b06333be008af601bc719a
alex <alexandre.baudouin161@gmail.com>
2024-09-24 15:48:57 +0200

    ajout du formulaire.php

---
+++ formulaire.php
@@ -0,0 +1,13 @@
+<!-- Bouton pour afficher le popup -->
+<button id="openPopupBtn">Connexion/Inscription</button>
+
+ <!-- Popup -->
+<div id="popup" class="popup-hidden">
+    <div class="popup-content">
+        <span id="closePopupBtn" class="close-btn">&times;</span>
+        <h2>Bienvenue</h2>
+        <p>Souhaitez-vous vous connecter ou vous inscrire ?</p>
+        <button class="popup-btn" id="loginBtn">Se connecter</button>
+        <button class="popup-btn" id="signupBtn">S'inscrire</button>
+    </div>
+</div>
