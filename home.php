e15d90c230aafd89d7cff4bb13ca54bcdf5f1f47
alex <alexandre.baudouin161@gmail.com>
2024-09-24 12:29:18 +0200

    ajout du footer dans le home.php

--- home.php
+++ home.php
@@ -5,9 +5,10 @@
     <title>GastronoMeal</title>
     <meta name="description" content="A brief description of your page.">
     <link rel="stylesheet" href="styles.css"> 
-   <!-- <link rel="icon" href="******.ico">  -->
+    <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
 </head>
 <body>
    
+<?php require_once('footer.php'); ?>
 </body>
 </html>
