062eb9bc0eafbc1389f2cb3eaa971b29982f7972
Alan <ladraitalan@gmail.com>
2024-09-24 12:34:09 +0200

    debut header html

--- header.php
+++ header.php
@@ -3,12 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/nav.css"/>
     <title>Document</title>
 </head>
 <body>
 <header>
-    <nav>
-        <img src="images/logo.webp" alt="logo" href="home.html"/>
+    <nav class="tp">
+        <div class="imgLogoNav">
+            <img src="images/logo.webp" alt="logo" href="home.html"/>
+        </div>
+        <div class="lienNav">
+            <a href="#">Menu</a>
+            <a href="#">Contact</a>
+            <div class="buttonNav">
+                <button href="#">Connexion</button>
+                <button href="#">Inscription</button>
+            </div>
+        </div>
+        
+
     </nav>
 </header>
 </body>
