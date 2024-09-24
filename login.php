dd03805e9bd2d3dec1fc5cacc8fd4566ca5e0efb
alex <alexandre.baudouin161@gmail.com>
2024-09-24 20:02:11 +0200

    Premiere mise en forme pour fonctionnalité du link popup

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>GastronoMeal</title>
+    <meta name="description" content="A brief description of your page.">
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- <link rel="stylesheet" href="css/.css"> -->
+    <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
+</head>
+<body>
+    <div class="containerPrincipal">
+       
+    </div>
+   
+<?php require_once('footer.php'); ?>
+</body>
+</html>
