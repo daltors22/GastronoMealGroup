a964bc7e9b0484a9e846b15d14a2660921f349f4
alex <alexandre.baudouin161@gmail.com>
2024-09-24 19:24:58 +0200

    ajout href= + link pages register + login sur boutton popup

--- formulaire.php
+++ formulaire.php
@@ -5,13 +5,14 @@
     <title>GastronoMeal</title>
     <link rel="stylesheet" href="css/formulaire.css"> 
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
-</head><!-- Popup -->
+</head>
+<!-- Popup -->
 <div id="popup" class="popup-hidden">
     <div class="popup-content">
         <h2>Bienvenue</h2>
         <p>Souhaitez-vous vous connecter ou vous inscrire ?</p>
-        <button class="popup-btn" id="loginBtn">Se connecter</button>
-        <button class="popup-btn" id="signupBtn">S'inscrire</button>
+        <button href="login.php" class="popup-btn" id="loginBtn">Se connecter</button>
+        <button href="register.php" class="popup-btn" id="signupBtn">S'inscrire</button>
     </div>
 </div>
 </html>
