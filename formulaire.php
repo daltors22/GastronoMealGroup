d44527f59aa5845983c9c6a91e4ea8274b26a10c
alex <alexandre.baudouin161@gmail.com>
2024-09-24 19:03:13 +0200

    MAJ formulaire.php - ajout du lien css

--- formulaire.php
+++ formulaire.php
@@ -1,13 +1,17 @@
-<!-- Bouton pour afficher le popup -->
-<button id="openPopupBtn">Connexion/Inscription</button>
-
- <!-- Popup -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>GastronoMeal</title>
+    <link rel="stylesheet" href="css/formulaire.css"> 
+    <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
+</head><!-- Popup -->
 <div id="popup" class="popup-hidden">
     <div class="popup-content">
-        <span id="closePopupBtn" class="close-btn">&times;</span>
         <h2>Bienvenue</h2>
         <p>Souhaitez-vous vous connecter ou vous inscrire ?</p>
         <button class="popup-btn" id="loginBtn">Se connecter</button>
         <button class="popup-btn" id="signupBtn">S'inscrire</button>
     </div>
 </div>
+</html>
