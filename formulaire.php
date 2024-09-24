7cb39c6a717c72156dde2c77c79062d4ebc6d2dd
alex <alexandre.baudouin161@gmail.com>
2024-09-24 20:01:36 +0200

    modif link vers login et vers register + link css

--- formulaire.php
+++ formulaire.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>GastronoMeal</title>
     <link rel="stylesheet" href="css/formulaire.css"> 
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
 </head>
 <!-- Popup -->
@@ -11,8 +12,8 @@
     <div class="popup-content">
         <h2>Bienvenue</h2>
         <p>Souhaitez-vous vous connecter ou vous inscrire ?</p>
-        <button href="login.php" class="popup-btn" id="loginBtn">Se connecter</button>
-        <button href="register.php" class="popup-btn" id="signupBtn">S'inscrire</button>
+        <a href="login.php"><button class="popup-btn" id="loginBtn">Se connecter</button></a>
+        <a href="register.php"><button class="popup-btn" id="signupBtn">S'inscrire</button></a>
     </div>
 </div>
 </html>
