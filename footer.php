5e630ac92be7590e131bfc7a46af28e97aeefacd
alex <alexandre.baudouin161@gmail.com>
2024-09-24 20:00:57 +0200

    modification de Me à Nous contacter

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer">
         <a href=""><p>Conditions Générales d'Utilisation (CGU) du Site Web</p></a>
-        <a href=""><p>Me contacter</p></a>
+        <a href=""><p>Nous contacter</p></a>
         <p>© 2024. Tous droits réservés.</p> 
     </div>  
 </footer>
