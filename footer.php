61d3839387f324a3f67379c8a5b25b75defb59ea
alex <alexandre.baudouin161@gmail.com>
2024-09-26 18:10:16 +0200

    Ajout du lien Home page dans footer pour avoir un retour a l'index le temps d'avoir le header+menu

--- footer.php
+++ footer.php
@@ -3,5 +3,7 @@
         <a href=""><p>Conditions Générales d'Utilisation (CGU) du Site Web</p></a>
         <a href=""><p>Nous contacter</p></a>
         <p>© 2024. Tous droits réservés.</p> 
+        <!-- LIEN TEMPORAIRE LE TEMPS D'AVOIR UN HEADER -->
+        <a href="home.php"><p>HOME PAGE</p></a>
     </div>  
 </footer>
