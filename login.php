0bf83d72de169668c3a45eddce67cc4a94caf590
alex <alexandre.baudouin161@gmail.com>
2024-09-26 19:46:36 +0200

    MAJ login, ajout form

--- login.php
+++ login.php
@@ -5,14 +5,25 @@
     <title>GastronoMeal</title>
     <meta name="description" content="A brief description of your page.">
     <link rel="stylesheet" href="css/styles.css">
-    <!-- <link rel="stylesheet" href="css/.css"> -->
+    <link rel="stylesheet" href="css/login.css?v=1.1">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
 </head>
 <body>
     <div class="containerPrincipal">
         <div class="content_login">
-            <button></button>
-            <button></button>
+            <h2>Connectez-vous</h2>
+            <p>Entrez votre adresse mail</p>
+            <form>
+                <label for="mail"></label>
+                <input type="text" id="mail" name="mail">
+            </form>
+            <form>
+                <p>Entrez votre mot de passe</p>
+                <label for="mot_de_passe"></label>
+                <input type="text" id="passwd" name="passwd">
+                <a href=""><p>mot de passe oublié ?</p></a>
+            </form>
+            <button>Connexion</button>   
         </div>
     </div>
    
