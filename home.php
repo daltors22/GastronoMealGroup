ed3931a6ecd49401564ac928bc15f4e2aae016a9
alex <alexandre.baudouin161@gmail.com>
2024-09-27 15:30:39 +0200

    Ajoute versionnage sur link css

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
     <title>GastronoMeal</title>
     <meta name="description" content="A brief description of your page.">
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/home.css?v=1.0">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
 </head>
 <body>
