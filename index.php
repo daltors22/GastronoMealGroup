d368316882da56b7c80c53924a6c42afc1775a47
alex <alexandre.baudouin161@gmail.com>
2024-09-27 16:02:06 +0200

    ajout du fichier index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>GastronoMeal</title>
+    <meta name="description" content="A brief description of your page.">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/home.css?v=1.0">
+    <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
+</head>
+<body>
+    <div class="containerPrincipal">
+        
+    </div>
+   
+<?php require_once('footer.php'); ?>
+</body>
+</html>
