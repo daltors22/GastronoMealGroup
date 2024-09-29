04bd83a43a293db89d5693fad7cd7713215087cc
alex <alexandre.baudouin161@gmail.com>
2024-09-29 09:55:24 +0200

    ajout du header + button close

--- login.php
+++ login.php
@@ -7,11 +7,14 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/login.css?v=1.5">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
+    <link rel="stylesheet" href="css/nav.css"/>
 </head>
 <body>
+    <?php require_once('header.php') ?>
     <div class="containerPrincipal">
       <div class="content_full">  
         <div class="content_login">
+        <a href="index.php" class="close_btn">&times;</a>
             <h2>Connectez-vous</h2>
             <p>Entrez votre adresse mail</p>
             <form>
