c9a5ad25abebd595da8b99a216983c3ae1d2d35b
alex <alexandre.baudouin161@gmail.com>
2024-09-29 10:17:06 +0200

    création d'un array test pour vérif email+nom+password

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php
+$users = [
+    [
+        'full_name' => 'Alexandre',
+        'email' => 'alexandre.baudouin161@gmail.com',
+        'password' => 'pass', 
+    ],
+    [
+        'full_name' => 'alan',
+        'email' => 'votreEmail@gmail.com',
+        'password' => 'pass',  
+    ],
+    [
+        'full_name' => 'Charane',
+        'email' => 'votreEmail@gmail.com',
+        'password' => 'pass',  
+    ],
+];
+?>
