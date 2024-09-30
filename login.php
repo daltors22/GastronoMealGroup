a9943da935b26394a36dbdec3fad1ba587b1b566
alex <alexandre.baudouin161@gmail.com>
2024-09-30 20:32:23 +0200

    resolution de bug sur le footer

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>GastronoMeal</title>
     <meta name="description" content="A brief description of your page.">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
     <link rel="stylesheet" href="css/login.css?v=1.8">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
     <link rel="stylesheet" href="css/nav.css"/>
@@ -37,7 +37,7 @@
                 <button class="envoyerForgot">Envoyer</button>
                 <button class="fermerForgot" @click="closeForgotPassword">Fermer</button>
             </div>
-        </ div>
+        </div>
       </div>
     </div>
  
