7ad05b30cdfe963b6fd18e75449c9f49f8bacac4
alex <alexandre.baudouin161@gmail.com>
2024-09-30 11:36:03 +0200

    Creation de new Vue pour le forgot password

--- login.php
+++ login.php
@@ -5,15 +5,16 @@
     <title>GastronoMeal</title>
     <meta name="description" content="A brief description of your page.">
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/login.css?v=1.5">
+    <link rel="stylesheet" href="css/login.css?v=1.6">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
     <link rel="stylesheet" href="css/nav.css"/>
 </head>
 <body>
     <?php require_once('header.php') ?>
+ 
     <div class="containerPrincipal">
-      <div class="content_full">  
-        <div class="content_login">
+      <div id="app" class="content_full">  
+        <div class="content_login" :class="{ close: isContentLoginVisible }">
         <a href="index.php" class="close_btn">&times;</a>
             <h2>Connectez-vous</h2>
             <p>Entrez votre adresse mail</p>
@@ -24,14 +25,45 @@
             <form>
                 <p>Entrez votre mot de passe</p>
                 <label for="mot_de_passe"></label>
-                <input type="text" id="passwd" name="passwd">
-                <a href=""><p>mot de passe oublié ?</p></a>
+                <input type="password" id="passwd" name="passwd"><br>
+                <a href="#" @click.prevent="showForgotPassword"><p>mot de passe oublié ?</p></a>
             </form>
             <button>Connexion</button>   
         </div>
+        <div class="modal" :class="{ show: isForgotPasswordVisible }">
+            <div class="modal_content">
+                <h2>Rénitialiser le mot de passe</h2>
+                <input type="email" placeholder="Entrez votre email"><br>
+                <button>Envoyer</button>
+                <button @click="closeForgotPassword">Fermer</button>
+            </div>
+        </div>
       </div>
     </div>
-   
-<?php require_once('footer.php'); ?>
+ 
+    <?php require_once('footer.php'); ?>
+    <!-- POST DEV POUR PROD <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
+    <script>
+        new Vue({
+            el: '#app',
+            
+            data: {
+                isForgotPasswordVisible: false,
+                isContentLoginVisible: true
+            },
+
+            methods:{
+                showForgotPassword(){
+                    this.isForgotPasswordVisible = true ;
+                // à finir pour cacher div content_login    this.isContentLoginVisible = false ;
+                },
+                closeForgotPassword(){
+                    this.isForgotPasswordVisible = false ;
+                },
+                
+            },
+        });    
+    </script>
 </body>
 </html>
