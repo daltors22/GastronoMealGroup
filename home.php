43a652a8db74675aaa3a2118673aa0d16c5a2bca
alex <alexandre.baudouin161@gmail.com>
2024-10-01 12:16:00 +0200

    maj css

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/home.css?v=1.0">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
-    <link rel="stylesheet" href="css/nav.css"/>
+    <link rel="stylesheet" href="css/nav.css?v=1.1"/>
 </head>
 <body>
     <?php require_once("header.php"); ?>
