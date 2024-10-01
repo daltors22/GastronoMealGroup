4e7dda726ad5932f54793bea093de2812ddbea7d
alex <alexandre.baudouin161@gmail.com>
2024-10-01 18:44:14 +0200

    header2.php mise en forme pour login

---
+++ header2.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="tp">
+        <div class="imgLogoNav">
+            <img src="images/G-meal-3.png" alt="logo" href="home.html"/>
+        </div>
+        <div class="lienNav">
+            <div class="lien">
+                <a href="index.php">Menu</a>
+                <a href="#">Contact</a>
+            </div>
+            <div class="buttonNav">
+                <a href="index.php"><button class="btnBlack buttonNavig">Déconnexion</button></a>
+            </div>
+            <div class="userAvatar">    
+                <a href="#"><img src="images/Pf-3.png" alt="Avatar utilisateur"></a>
+            </div>
+        </div>
+
+    </nav>
+</header>
