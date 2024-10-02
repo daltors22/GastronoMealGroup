c7754384cb2fbbaa2bd78d441500240365b59fa0
alex <alexandre.baudouin161@gmail.com>
2024-10-02 13:03:54 +0200

    ajout du profil client.php

---
+++ profilClient.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>GastronoMeal</title>
+    <meta name="description" content="A brief description of your page.">
+    <link rel="stylesheet" href="css/styles.css?v=1.4">
+    <link rel="stylesheet" href="css/home.css?v=1.0">
+    <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
+    <link rel="stylesheet" href="css/isLogin.css?v=1.0"/>
+    <link rel="stylesheet" href="css/header2.css?v=1.0"/>
+</head>
+<body>
+    <?php require_once("header2.php"); ?>
+    <div class="containerPrincipal">
+        <div class="container_1">
+            
+        </div>
+        <div class="container_2">
+           
+        </div>
+    </div>
+   
+    <?php require_once('footer.php'); ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
+</body>
+</html>
