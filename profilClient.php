f56eb279e13386a3b3fdee8b69e25f4535008931
alex <alexandre.baudouin161@gmail.com>
2024-10-03 12:33:26 +0200

    Maj ajout button + prepa 2eme div

--- profilClient.php
+++ profilClient.php
@@ -4,22 +4,20 @@
     <meta charset="UTF-8">
     <title>GastronoMeal</title>
     <meta name="description" content="A brief description of your page.">
-    <link rel="stylesheet" href="css/styles.css?v=1.5">
+    <link rel="stylesheet" href="css/styles.css?v=1.6">
     <link rel="stylesheet" href="css/home.css?v=1.0">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
     <link rel="stylesheet" href="css/isLogin.css?v=1.0"/>
     <link rel="stylesheet" href="css/header2.css?v=1.2"/>
-    <link rel="stylesheet" href="css/profilClient.css?v=1.3"/>
+    <link rel="stylesheet" href="css/profilClient.css?v=1.4"/>
 </head>
 <body>
     <?php require_once("header2.php"); ?>
     <div class="containerPrincipal">
         <div class="container_1_profilClient">
-        <h3>Mon profil</h3>
+            <h3>Mon profil</h3>
         <br>
             <div class="containerProfil">
-                
-                
                 <div class="containerAdresse">
                     <h3><img src="images/information.png" alt="infos icone">&nbsp;&nbsp; Votre adresse</h3>
                 <br>
@@ -34,11 +32,14 @@
                 <br>
                     <button class="btnAdresse">Modifier</button>
                 </div>
-            </div>
+            </div> 
+            <div class="HabitudeClient">
+            <a href="#"><h3 class="mesAchats">Mes achats</h3></a>
             
+            <a href="#"><h3 class="mesPref">Mes préférences</h3></a>
         </div>
-        <h3>Vos achats</h3>
-            <h3>Mes préférencers</h3>
+        </div>
+        
     </div>
    
     <?php require_once('footer.php'); ?>
