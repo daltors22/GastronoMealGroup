71a06d1abedbfbe29f64750f6095a92cbc2dd243
alex <alexandre.baudouin161@gmail.com>
2024-10-10 09:47:57 +0200

    essaie text deco none

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer">
-        <a href=""><p>Conditions Générales d'Utilisation (CGU) du Site Web</p></a>
-        <a href=""><p>Nous contacter</p></a>
+        <a href="#"><p>Conditions Générales d'Utilisation (CGU) du Site Web</p></a>
+        <a href="#"><p>Nous contacter</p></a>
         <p>© 2024. Tous droits réservés.</p> 
         <!-- LIEN TEMPORAIRE LE TEMPS D'AVOIR UN HEADER -->
         <a href="home.php"><p>HOME PAGE</p></a>
