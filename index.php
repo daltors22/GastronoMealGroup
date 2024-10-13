be1b07f94165640d29503ccb1d116098e4c50655
alex <alexandre.baudouin161@gmail.com>
2024-10-13 13:35:57 +0200

    préparation home page avec fond + recherche restaurant ( non terminé )

--- index.php
+++ index.php
@@ -4,22 +4,29 @@
     <meta charset="UTF-8">
     <title>GastronoMeal</title>
     <meta name="description" content="A brief description of your page.">
-    <link rel="stylesheet" href="css/styles.css?v=2.0">
+    <link rel="stylesheet" href="css/styles.css?v=2.1">
     <link rel="icon" href="GastronoMealGroup/images/G-meal-2.ico">
 </head>
 <body>
     <?php require_once("header.php"); ?>
     <div class="containerPrincipal">
         <div class="container_1">
-            <h1>Bienvenue</h1>
-            <h3>sur le site</h3>
-            <div class="button_container">
-            <a href="login.php"><button class="button_connexion">Connexion</button></a>
-            <a href="register.php"><button class="button_inscription">inscription</button></a>
-            </div>
         </div>
         <div class="container_2">
-           
+            <div class="superpoImage">
+                <div class="foundAdress"></div>
+                <div class="whenLivraison"></div>
+                <button class="foundRetaurantBtn">Trouver un restaurant</button>
+            </div>
+            <img src="images/foodAlex.png" alt="image restaurant" class="imgRestaurantIndex">
+        </div>
+    </div>
+    <div class="containerSecondaire">
+        <div class="container_1">
+            
+        </div>
+        <div class="container_2">
+          
         </div>
     </div>
    
